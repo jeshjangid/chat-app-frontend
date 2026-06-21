46fa7689800276c56000f227079ae44ae3c072ce
jeshjangid <jeshjangid09@gmail.com>
2026-06-21 19:30:43 +0530

    home update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from 'react'
-import Sidebar from '../components/Sidebar'
-import ChatContainer from '../components/ChatContainer'
-import RightSidebar from '../components/RightSidebar'
-import { ChatContext } from '../context/ChatContext'
+import Sidebar from '../components/Sidebar.jsx'
+import ChatContainer from '../components/ChatContainer.jsx'
+import RightSidebar from '../components/RightSidebar.jsx'
+import { ChatContext } from '../context/ChatContext.jsx'
 
 const HomePage = () => {
 
@@ -11,7 +11,7 @@ const HomePage = () => {
     <div className="w-full h-screen border sm:px-[15%] sm:py-[5%]">
         <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
             <Sidebar />
-            <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+            <ChatContainer />
             <RightSidebar />
         </div>
     </div>
